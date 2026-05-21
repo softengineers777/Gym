56b6c1bd98e189a926f877ec0de641e16a7aa2de
John Jamer Bolivar Vargas <jjbolivar@eliteflower.com>
2026-05-21 15:46:54 -0500

    Se  realiza  cambios  en  el controlador de  Compras

--- Controllers/ComprasController.cs
+++ Controllers/ComprasController.cs
@@ -15,7 +15,7 @@ namespace GuayabitosMvc.Controllers
 
         {
             _context = context;
-            _configuration = configuration;       
+            _configuration = configuration;
         }
         public async Task<IActionResult> Index()
         {
@@ -37,13 +37,13 @@ namespace GuayabitosMvc.Controllers
             return View(compras);
         }
         [HttpGet]
-        public  IActionResult Create()
+        public IActionResult Create()
         {
             return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create ([Bind("IdCompras,IdProveedor,fecha_compra,total,estado")]Compras compras)
+        public async Task<IActionResult> Create([Bind("IdCompras,IdProveedor,fecha_compra,total,estado")] Compras compras)
         {
             if (ModelState.IsValid)
             {
@@ -53,7 +53,86 @@ namespace GuayabitosMvc.Controllers
             }
             return View(compras);
         }
-        
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var compras = await _context.Compras.FindAsync(id);
+            if (compras == null)
+            {
+                return NotFound();
+            }
+            return View(compras);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit (int id, [Bind("IdCompras,IdProveedor,fecha_compra,total,estado")]Compras compras)
+        {
+            if (id != compras.IdCompras)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(compras);
+                    await _context.SaveChangesAsync();
+                }
+                catch(DbUpdateConcurrencyException)
+                {
+                    if (!ComprasExists(compras.IdCompras))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                RedirectToAction(nameof(Index));
+            }
+            return View (compras);
+            
+        }
+        public bool ComprasExists(int id)
+        {
+             return _context.Compras.Any(c => c.IdCompras == id);
+        }
+        [HttpGet, ActionName("Delete")]
+        public  async Task <IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var compras = await _context.Compras
+            .FirstOrDefaultAsync(c =>c.IdCompras == id);
+            if(compras == null)
+            {
+                return NotFound();
+            }
+            return View(compras);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var compras = await _context.Compras.FindAsync(id);
+            if (compras == null)
+            {
+                 return NotFound(); 
+            }
+            _context.Compras.Remove(compras);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
 
 
 
