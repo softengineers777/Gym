5fc72e72364c2e0b7bf810d9690df92cb46f014c
John Jamer Bolivar Vargas <jjbolivar@eliteflower.com>
2026-06-19 09:26:07 -0500

    Se realiza  el controlador de Pedidos_Detalle

--- Controllers/Pedidos_DetalleController.cs
+++ Controllers/Pedidos_DetalleController.cs
@@ -6,29 +6,29 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GuayabitosMvc.Controllers
 {
-    public class Pedidos_DetalleController: Controller
+    public class Pedidos_DetalleController : Controller
     {
         private readonly GuayabitosDbContext _context;
         private readonly IConfiguration _configuration;
 
-        public  Pedidos_DetalleController(GuayabitosDbContext context,IConfiguration configuration)
+        public Pedidos_DetalleController(GuayabitosDbContext context, IConfiguration configuration)
         {
             _context = context;
             _configuration = configuration;
         }
         public async Task<IActionResult> Index()
         {
-            return View (await _context.Pedidos_Detalle.ToListAsync());
+            return View(await _context.Pedidos_Detalle.ToListAsync());
         }
-        public async Task <IActionResult>Details(int?  id)
+        public async Task<IActionResult> Details(int? id)
         {
-            if(id== null || _context.Pedidos_Detalle == null)
+            if (id == null || _context.Pedidos_Detalle == null)
             {
-                return  NotFound();
+                return NotFound();
             }
             var pedidos_detalle = await _context.Pedidos_Detalle
-            .FirstOrDefaultAsync(pd =>pd.IdDetallePedido== id);
-            if(pedidos_detalle == null)
+            .FirstOrDefaultAsync(pd => pd.IdDetallePedido == id);
+            if (pedidos_detalle == null)
             {
                 return NotFound();
             }
@@ -41,7 +41,7 @@ namespace GuayabitosMvc.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public  async Task<IActionResult> CreateMultiple(List<Pedidos_Detalle>pedidos_detalle)
+        public async Task<IActionResult> CreateMultiple(List<Pedidos_Detalle> pedidos_detalle)
         {
             try
             {
@@ -50,13 +50,94 @@ namespace GuayabitosMvc.Controllers
                     return RedirectToAction(nameof(Index));
                 }
                 Console.WriteLine($"Pedido detall no es  null {pedidos_detalle.Count}");
-                for (int i = 0; i <pedidos_detalle.Count; i++)
+                for (int i = 0; i < pedidos_detalle.Count; i++)
                 {
-                    var pd= pedidos_detalle[i];
+                    var pd = pedidos_detalle[i];
                     // Console.WriteLine()
                 }
-                var 
+                // var pedidos_detalleValidos = pedidos_detalle.Where (c=> !String.IsNullOrEmpty(c.Nombre)).Tolist();
+                // if(pedidos_detalleValidos.Any())
+                // {
+                //     foreach(pedidos_detalleValidos.)
+                // }
+
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error:{ex.Message}");
+                Console.WriteLine($"detalle:{ex.StackTrace}");
+                TempData["Error"] = $"Error {ex.Message}";
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var pedidos_detalle = await _context.Pedidos_Detalle.FindAsync(id);
+            if (pedidos_detalle == null)
+            {
+                return NotFound();
+            }
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdDetallePedido,IdPedido,IdProducto,Cantidad,PrecioUnitario,SubTotal,Observaciones,Estado")] Pedidos_Detalle pedidos_detalle)
+        {
+            if (id != pedidos_detalle.IdDetallePedido)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pedidos_detalle);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!Pedidos_DetalleExists(pedidos_detalle.IdDetallePedido))
+                    {
+                        return NotFound();
+                    }
+                }
+                return RedirectToAction(nameof(Edit));
+            }
+            return View(pedidos_detalle);
+        }
+        public bool Pedidos_DetalleExists(int? id)
+        {
+            return _context.Pedidos_Detalle.Any(pd => pd.IdDetallePedido == id);
+        }
+        [HttpGet]
+        public async Task<IActionResult>Delete(int id )
+        {
+            var  pedidos_detalle = await _context.Pedidos_Detalle.FindAsync(id);
+            if (pedidos_detalle ==null)
+            {
+                return NotFound();
+            }
+            _context.Pedidos_Detalle.Remove(pedidos_detalle);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pedidos_detalle = await  _context.Pedidos_Detalle.FindAsync(id);
+            if(pedidos_detalle == null)
+            {
+                return NotFound();
+            }
+            _context.Pedidos_Detalle.Remove(pedidos_detalle);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
